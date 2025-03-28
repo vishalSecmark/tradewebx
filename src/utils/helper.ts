d73d99a03ddb092b55d77b5809b50f36ad4635e5
Ashu Kumar <kumar.ashu414283@gmail.com>
2025-03-28 12:42:10 +0530

    Persist themes on logout

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,7 +1,12 @@
+import { THEME_COLORS_STORAGE_KEY, THEME_STORAGE_KEY } from "@/context/ThemeContext";
 import { APP_METADATA_KEY } from "./constants";
 
 export const clearLocalStorage = () => {
     const appMetadata = localStorage.getItem(APP_METADATA_KEY);
+    const appThemeColors = localStorage.getItem(THEME_COLORS_STORAGE_KEY);
+    const appTheme = localStorage.getItem(THEME_STORAGE_KEY);
     localStorage.clear();
     localStorage.setItem(APP_METADATA_KEY, appMetadata);
+    if (appThemeColors) localStorage.setItem(THEME_COLORS_STORAGE_KEY, appThemeColors);
+    if (appTheme) localStorage.setItem(THEME_STORAGE_KEY, appTheme);
 }
