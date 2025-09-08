f758497d7322eac0137aa4e6d56d84b24ecde569
vishalSecmark <vishalsecmark@gmail.com>
2025-09-08 11:15:06 +0530

    delete toast added

--- src/components/EntryFormModal.tsx
+++ src/components/EntryFormModal.tsx
@@ -975,6 +975,7 @@ const EntryFormModal: React.FC<EntryFormModalProps> = ({ isOpen, onClose, pageDa
             if (response?.data?.success) {
                 fetchMasterEntryData(masterFormValues)
                 setIsConfirmationModalOpen(false);
+                toast.success('Child Record Deleted')
             }
 
         } catch (error) {
