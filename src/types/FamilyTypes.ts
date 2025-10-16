124d75b417293105468e69ecab7ae13c67851f7f
pavan <pavanpotdar788@gmail.com>
2025-10-16 16:10:32 +0530

    Type_Defined

---
+++ src/types/FamilyTypes.ts
@@ -0,0 +1,11 @@
+export interface FamilyRow {
+  id: number;
+  FamilyHead: string;
+  ClientCode: string;
+  ClientName: string;
+}
+
+export interface LoginData {
+  userId: string;
+  password: string;
+}
