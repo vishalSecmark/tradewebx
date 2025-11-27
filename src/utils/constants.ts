990eb0407113acb8e4bb7d99c1334a11de12aa16
ersimransingh <admin@simransingh.in>
2025-11-27 10:44:41 +0530

    fix: correct NEXT_PUBLIC_FULL_URL calculation to ensure proper URL formation

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -20,7 +20,7 @@ export const LOGIN_AS_OPTIONS = process.env.NEXT_PUBLIC_LOGIN_AS_OPTIONS
 export const LOGIN_KEY = process.env.NEXT_PUBLIC_LOGIN_KEY
 export const LOGIN_AS = process.env.NEXT_PUBLIC_LOGIN_AS
 export const BASE_PATH_FRONT_END = process.env.NEXT_PUBLIC_BASE_PATH || ''
-export const NEXT_PUBLIC_FULL_URL = getBaseURL() + process.env.NEXT_PUBLIC_BASE_PATH || ''
+export const NEXT_PUBLIC_FULL_URL = getBaseURL() + (process.env.NEXT_PUBLIC_BASE_PATH || '')
 export const SSO_URL = process.env.NEXT_PUBLIC_SSO_URL || '/TradeWebAPI/api/Main/Login_SSO'
 export const VERSION = "2.0.0.1"
 export const ENABLE_CAPTCHA = process.env.NEXT_PUBLIC_ENABLE_CAPTCHA !== 'false' // Default to true if not set to 'false'
